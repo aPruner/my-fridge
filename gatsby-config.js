7abcf47f084ee40cbb1275b5c79f717835adf892
aPruner <adampruner99@gmail.com>
2020-06-28 11:56:55 -0700

    fix gatsby typescript plugin issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,14 +28,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-typescript`,
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
