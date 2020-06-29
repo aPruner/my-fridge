7b66dfac385ba99a76c33e60bbf9ab5040717035
Johnny <johnnyliao06@gmail.com>
2020-06-28 19:41:13 -0700

    completed login mockup

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -11,13 +11,15 @@ import Dialog from "@material-ui/core/Dialog"
 import PersonIcon from "@material-ui/icons/Person"
 import AddIcon from "@material-ui/icons/Add"
 import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
+import GoogleButton from "react-google-button"
 import { blue } from "@material-ui/core/colors"
 
-const emails = ["username@gmail.com", "user02@gmail.com"]
 const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
+  signInTitle: {
+    paddingTop: "16px",
+    paddingLeft: "24px",
+    paddingRight: "24px",
   },
 })
 
@@ -35,43 +37,29 @@ function SimpleDialog(props: SimpleDialogProps) {
     onClose(selectedValue)
   }
 
-  const handleListItemClick = (value: string) => {
-    onClose(value)
-  }
-
   return (
     <Dialog
       onClose={handleClose}
       aria-labelledby="simple-dialog-title"
       open={open}
     >
-      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+      <DialogTitle className="signInTitle" id="simple-dialog-title">
+        Sign in
+      </DialogTitle>
       <List>
-        {emails.map(email => (
-          <ListItem
-            button
-            onClick={() => handleListItemClick(email)}
-            key={email}
-          >
-            <ListItemAvatar>
-              <Avatar className={classes.avatar}>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={email} />
-          </ListItem>
-        ))}
-        <ListItem
-          autoFocus
-          button
-          onClick={() => handleListItemClick("addAccount")}
-        >
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
+        <ListItem>
+          <TextField label="Username"></TextField>
+        </ListItem>
+        <ListItem>
+          <TextField label="Password" type="password"></TextField>
+        </ListItem>
+        <ListItem>
+          <Button variant="contained" color="primary">
+            LOGIN
+          </Button>
+        </ListItem>
+        <ListItem>
+          <GoogleButton />
         </ListItem>
       </List>
     </Dialog>
@@ -80,7 +68,7 @@ function SimpleDialog(props: SimpleDialogProps) {
 
 export default function SimpleDialogDemo() {
   const [open, setOpen] = React.useState(false)
-  const [selectedValue, setSelectedValue] = React.useState(emails[1])
+  const [selectedValue, setSelectedValue] = React.useState()
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -88,7 +76,6 @@ export default function SimpleDialogDemo() {
 
   const handleClose = (value: string) => {
     setOpen(false)
-    setSelectedValue(value)
   }
 
   return (
@@ -96,7 +83,7 @@ export default function SimpleDialogDemo() {
       <Typography variant="subtitle1">Selected: {selectedValue}</Typography>
       <br />
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Sign in with Google
+        Sign in
       </Button>
       <SimpleDialog
         selectedValue={selectedValue}
