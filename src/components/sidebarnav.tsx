5cec34252f07305f61af8117b4fd580025bdc0bb
Johnny <johnnyliao06@gmail.com>
2020-07-01 16:40:45 -0700

    finished sidebar nav

--- src/components/sidebarnav.tsx
+++ src/components/sidebarnav.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import { Drawer } from '@material-ui/core';
 
+// Material UI imports
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+import {
+  Home as HomeIcon,
+  ShoppingCart as ShoppingCartIcon,
+  Kitchen as KitchenIcon,
+} from '@material-ui/icons';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    paper: { width: '300px' },
+  })
+);
 const Sidebarnav: React.FC<SidebarnavProps> = ({ open, onClose }) => {
-  return <Drawer anchor="left" open={open} onClose={onClose} />;
+  const classes = useStyles();
+  return (
+    <Drawer
+      classes={{ paper: classes.paper }}
+      anchor="left"
+      open={open}
+      onClose={onClose}
+    >
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <HomeIcon fontSize="large" />
+            <ListItemText>Home</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+
+        <ListItem button>
+          <ListItemIcon>
+            <ShoppingCartIcon fontSize="large" />
+            <ListItemText>Shopping List</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+
+        <ListItem button>
+          <ListItemIcon>
+            <KitchenIcon fontSize="large" />
+            <ListItemText>Fridge</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 };
 
 export interface SidebarnavProps {
