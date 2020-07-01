cbe31a928d68fbd216bff40ac0cc7f936b402492
aPruner <adampruner99@gmail.com>
2020-07-01 16:07:10 -0700

    get rid of layout and SEO tests as they aren't important

--- src/tests/components/layout.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Layout from '../../components/layout';
-
-test('should render Layout component correctly', () => {
-  const wrapper = shallow(<Layout siteTitle="MyFridge" />);
-  expect(wrapper).toMatchSnapshot();
-});
