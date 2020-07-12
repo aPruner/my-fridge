be0a9fb90031bab053f8546dc55a682d7ea60bcc
aPruner <adampruner99@gmail.com>
2020-07-11 22:37:58 -0700

    write dispatcher wrappers for login and logout, need to hook the signinDialog into redux next

--- src/state/actions/auth.ts
+++ src/state/actions/auth.ts
@@ -1,19 +1,50 @@
+import { Dispatch } from 'react';
+
+import {
+  signinWithGoogle,
+  signOut,
+} from '../../utils/services/firebaseAuthService';
+
 // LOGIN:
+
+// Login action dispatcher
+export function loginActionDispatcher(withGoogle: boolean): LoginDispatcher {
+  return (dispatch: Dispatch<LoginAction>): void => {
+    dispatch(loginRequest());
+    if (withGoogle) {
+      signinWithGoogle()
+        .then((credential) => {
+          dispatch(loginSuccess(credential));
+          console.log(credential);
+        })
+        .catch((error) => {
+          dispatch(loginFailure());
+          console.log(error);
+        });
+    } else {
+      // TODO: Fill in login with user/pass once it's implemented
+    }
+  };
+}
+
 // Action type string constants
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
-// Action functions
+// Action function wrappers
 export function loginRequest(): LoginRequest {
   return {
     type: LOGIN_REQUEST,
   };
 }
 
-export function loginSuccess(): LoginSuccess {
+export function loginSuccess(
+  credential: firebase.auth.UserCredential
+): LoginSuccess {
   return {
     type: LOGIN_SUCCESS,
+    credential,
   };
 }
 
@@ -24,12 +55,17 @@ export function loginFailure(): LoginFailure {
 }
 
 // Action TS types
+export interface LoginDispatcher {
+  (dispatch: Dispatch<LoginAction>): void;
+}
+
 export interface LoginRequest {
   type: typeof LOGIN_REQUEST;
 }
 
 export interface LoginSuccess {
   type: typeof LOGIN_SUCCESS;
+  credential: firebase.auth.UserCredential;
 }
 
 export interface LoginFailure {
@@ -39,41 +75,62 @@ export interface LoginFailure {
 export type LoginAction = LoginRequest | LoginSuccess | LoginFailure;
 
 // LOGOUT:
+
+// Logout action dispatcher
+export function logoutActionDispatcher(): LogoutDispatcher {
+  return (dispatch: Dispatch<LogoutAction>): void => {
+    dispatch(logoutRequest());
+    signOut()
+      .then(() => {
+        dispatch(logoutSuccess());
+        console.log('logout success!');
+      })
+      .catch((error) => {
+        dispatch(logoutFailure());
+        console.log(error);
+      });
+  };
+}
+
 // Action type string constants
 export const LOGOUT_REQUEST = 'LOGIN_REQUEST';
 export const LOGOUT_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGOUT_FAILURE = 'LOGIN_FAILURE';
 
-// Action functions
-export function logoutRequest(): LoginRequest {
+// Action function wrappers
+export function logoutRequest(): LogoutRequest {
   return {
-    type: LOGIN_REQUEST,
+    type: LOGOUT_REQUEST,
   };
 }
 
-export function logoutSuccess(): LoginSuccess {
+export function logoutSuccess(): LogoutSuccess {
   return {
-    type: LOGIN_SUCCESS,
+    type: LOGOUT_SUCCESS,
   };
 }
 
-export function logoutFailure(): LoginFailure {
+export function logoutFailure(): LogoutFailure {
   return {
-    type: LOGIN_FAILURE,
+    type: LOGOUT_FAILURE,
   };
 }
 
 // Action TS types
+export interface LogoutDispatcher {
+  (dispatch: Dispatch<LogoutAction>): void;
+}
+
 export interface LogoutRequest {
-  type: typeof LOGIN_REQUEST;
+  type: typeof LOGOUT_REQUEST;
 }
 
 export interface LogoutSuccess {
-  type: typeof LOGIN_SUCCESS;
+  type: typeof LOGOUT_SUCCESS;
 }
 
 export interface LogoutFailure {
-  type: typeof LOGIN_FAILURE;
+  type: typeof LOGOUT_FAILURE;
 }
 
 export type LogoutAction = LogoutRequest | LogoutSuccess | LogoutFailure;
